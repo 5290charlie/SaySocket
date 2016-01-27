db143854c32c55cc1576935abb89f039857182b0
Charlie McClung <charlie.mcclung@confidentfs.com>
2016-01-26 18:35:26 -0700

    Refactored video logic

--- public/v.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once '../inc/config.inc';
-
-if (isset($_GET['v']) && preg_match(REGEX_VIDEO_ID, $_GET['v'])) {
-  $ss_config['VIDEO_ID'] = $_GET['v'];
-  echo parse_file_contents('video.html');
-}
